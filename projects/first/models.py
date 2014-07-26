87235a8c5977f0495bbdf599632e9f16ed0ead39
aliouba <alioubafr@gmail.com>
2014-07-26 18:02:41 +0300

    Ajout Forms

--- projects/first/models.py
+++ projects/first/models.py
@@ -13,4 +13,16 @@ class Entreprises(models.Model):
     logo_entreprise = models.ImageField(null=True,upload_to="logo/entreprise")
     user = models.ForeignKey(User)
     def __unicode__(self):
-        return u"%s" % self.nom
+		return u"%s" % self.nom
+class Clients(models.Model):
+    nom = models.CharField(max_length=45)
+    prenom = models.CharField(max_length=45)
+    pays = models.CharField(max_length=45)
+    region = models.CharField(max_length=45)
+    ville = models.CharField(max_length=45)
+    commune = models.CharField(max_length=45)
+    adresse = models.CharField(max_length=255)
+    entreprise = models.ForeignKey(Entreprises)
+    code = models.CharField(max_length=10)
+    def __unicode__(self):
+		return u"%s" % self.nom
