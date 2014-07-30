463dfeb1fd47e7e7485644a42d6d2ad020ec4d4a
VirtualBoxImages.com <alioubafr@gmail.com>
2014-07-30 16:29:35 +0300

    Autre version d'ajout client

--- projects/first/views.py
+++ projects/first/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from first.forms import ArticleForm
 def add_client(request):
-    client_form = ArticleForm()
-    return render_to_response('first/add_client.html', {'add_client' : client_form})
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('/thanks/')
+    else:
+		form = ArticleForm()
+		print "FALSE"
+    return render(request, 'first/add_client.html', {'add_client': form})
