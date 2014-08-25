00a67e50d1893aa4c7a7ff4e33306603365f4166
VirtualBoxImages.com <alioubafr@gmail.com>
2014-08-25 09:49:14 +0300

    AJOUT CLIENT

--- projects/first/forms.py
+++ projects/first/forms.py
@@ -1,11 +1,25 @@
 from django import forms
 from django.forms import ModelForm
 from first.models import Clients
+from first.models import Entreprises
 class ArticleForm(ModelForm):
-	class Meta:
-		model = Clients
-		fields = ['nom', 'prenom', 'adresse','pays','region', 'code']
-		widgets = {
-                      "nom":forms.TextInput(attrs={'placeholder':'Name','name':'Name','id':'common_id_for_imputfields','class':'input-class_name'}),
-                      "prenom":forms.TextInput(attrs={'placeholder':'description','name':'description','id':'common_id_for_imputfields','class':'input-class_name'}),
+    code2 = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Verifier Mot de Passe','class':'form-control'}))
+    class Meta:
+        model = Clients
+	widgets = {
+                  "nom":forms.TextInput(attrs={'placeholder':'Name','class':'form-control'}),
+                 "prenom":forms.TextInput(attrs={'placeholder':'Prenom','class':'form-control'}),
+                  "date_naissance":forms.TextInput(attrs={'placeholder':'Date de Naissance','class':'form-control'}),
+                 "telephone":forms.TextInput(attrs={'placeholder':'Telephone','class':'form-control'}),
+                  "email":forms.TextInput(attrs={'placeholder':'Email','class':'form-control'}),
+                 "pays":forms.TextInput(attrs={'placeholder':'Pays','class':'form-control'}),
+                  "region":forms.TextInput(attrs={'placeholder':'Region','class':'form-control'}),
+                 "ville":forms.TextInput(attrs={'placeholder':'Ville','class':'form-control'}),
+                 "commune":forms.TextInput(attrs={'placeholder':'Commune','class':'form-control'}),
+                  "adresse":forms.TextInput(attrs={'placeholder':'Adresse','class':'form-control'}),
+                 "code":forms.TextInput(attrs={'placeholder':'Mot de Passe ','class':'form-control'}),
         }
+    def __init__(self, *args, **kwargs):
+	    super(ArticleForm, self).__init__(*args, **kwargs)
+	    self.fields["entreprise"].initial=1
+	    
--- projects/first/views.py
+++ projects/first/views.py
@@ -4,10 +4,17 @@ from django.shortcuts import render_to_response
 from first.forms import ArticleForm
 def add_client(request):
     if request.method == 'POST':
+        print "form valided"
         form = ArticleForm(request.POST)
         if form.is_valid():
+            print "form valid"
+            data = form.save(commit=False)		
+            print "yes"
             return HttpResponseRedirect('/thanks/')
+        else:
+			print "non valid"        
     else:
-		form = ArticleForm()
-		print "FALSE"
+        print "FALSE"
+        print request.user.id
+        form = ArticleForm()
     return render(request, 'first/add_client.html', {'add_client': form})
